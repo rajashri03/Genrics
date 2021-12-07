3b54007d0e80273a97cb5af81ad05b923e0b52c8
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-07 14:17:10 +0530

    Find Maximum between the three integers

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Generics;
+
+// Creating an object using 'new'
+// Calling the parameterized constructor With parameters 2,3,4
+Integer_Max_Num max = new Integer_Max_Num(2,7,5);
+int result =max.FindMax();
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("Maximum Number is:" + result);// To display Maximum Number
+Console.WriteLine("-----------------------------------");
