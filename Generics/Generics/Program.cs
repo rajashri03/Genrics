566c696019bdfd00f50cfc07bf29bcc079d4a97e
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-12 17:21:34 +0530

    Using Generic Method

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -5,7 +5,11 @@ bool check = true;
 MaxNum max = new MaxNum();
 while (check)
 {
-    Console.Write("Select Option:\n1)Find Integer Maximum Number\n2)Find Float Maximum Number\n3)Find Maximum String");
+    Console.Write("Select Option:\n" +
+        "1)Find Integer Maximum Number\n" +
+        "2)Find Float Maximum Number\n" +
+        "3)Find Maximum String\n" +
+        "4)Refactor All to One Generic Method\n");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -25,6 +29,19 @@ while (check)
             string result2 = max.FindMax_String("Apple","Peach","Banana");
             Console.WriteLine("-----------------------------------");
             Console.WriteLine("Maximum String is:" + result2);// To display Maximum Number
+            Console.WriteLine("-----------------------------------");
+            break;
+        case 4:
+            Using_GenericMethod refactor = new Using_GenericMethod();
+            int intresult=refactor.FindMax<int>(2, 3, 4);
+            string stringresult=refactor.FindMax<string>("Apple", "Banana", "Peach");
+            float floatresult=refactor.FindMax<float>(2.7f, 3.4f, 6.4f);
+            Console.WriteLine("-------------Int Max----------------------");
+            Console.WriteLine("Maximum Interger is:" + intresult);
+            Console.WriteLine("\n-------------String Max----------------------");
+            Console.WriteLine("Maximum String is:" + stringresult);
+            Console.WriteLine("\n-------------Float Max----------------------");
+            Console.WriteLine("Maximum Float Number  is:" + floatresult);
             Console.WriteLine("-----------------------------------");
             break;
         default:
---
+++ Generics/Generics/Using_GenericMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Using_GenericMethod
+    {
+        public T FindMax<T>(T First,T Second,T Third) where T :IComparable<T>
+        {
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
+            {
+                return First;
+            }
+            if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
+            {
+                return Second;
+            }
+            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
+            {
+                return Third;
+            }
+            return default;
+        }
+    }
+}
